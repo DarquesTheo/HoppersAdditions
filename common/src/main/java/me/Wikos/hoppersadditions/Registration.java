0d97cb0c137446da58d3bf5d86744975256504da
Theo Darques <theo.darques@epitech.eu>
2026-03-27 22:39:18 +0100

    Added pickaxe hopper model && recipe

--- common/src/main/java/me/Wikos/hoppersadditions/Registration.java
+++ common/src/main/java/me/Wikos/hoppersadditions/Registration.java
@@ -8,8 +8,11 @@ import me.Wikos.hoppersadditions.BlockEntity.PickaxeHopperBlockEntity;
 import me.Wikos.hoppersadditions.BlockEntity.ReinforcedHopperBlockEntity;
 import me.Wikos.hoppersadditions.Item.PickaxeHopperItem;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -21,6 +24,7 @@ public class Registration {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, Registries.BLOCK);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(MOD_ID, Registries.BLOCK_ENTITY_TYPE);
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(MOD_ID, Registries.CREATIVE_MODE_TAB);
 
     public static final RegistrySupplier<Block> REINFORCED_HOPPER = BLOCKS.register("reinforced_hopper",
             () -> new ReinforcedHopperBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.HOPPER)));
@@ -42,9 +46,21 @@ public class Registration {
             BLOCK_ENTITIES.register("pickaxe_hopper", () ->
                     BlockEntityType.Builder.of(PickaxeHopperBlockEntity::new, PICKAXE_HOPPER.get()).build(null));
 
+    public static final RegistrySupplier<CreativeModeTab> HOPPER_TAB = TABS.register("hoppers_tab", () ->
+            CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
+                    .title(Component.translatable("itemGroup." + MOD_ID + ".hoppers_tab"))
+                    .icon(() -> new ItemStack(REINFORCED_HOPPER.get()))
+                    .displayItems((parameters, output) -> {
+                        output.accept(REINFORCED_HOPPER_ITEM.get());
+                        output.accept(PICKAXE_HOPPER_ITEM.get());
+                    })
+                    .build()
+    );
+
     public static void init() {
         BLOCKS.register();
         ITEMS.register();
         BLOCK_ENTITIES.register();
+        TABS.register();
     }
 }
